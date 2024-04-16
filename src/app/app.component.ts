ae18c4993dd550dcad48555b64606e933ecb665a
OussamaAalailiev <aalaila.oussama@gmail.com>
2024-04-16 11:12:56 +0100

    initial commit using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ProductFeatureComponent } from './product-feature/product-feature.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, ProductFeatureComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'casa-learning-center';
---
+++ src/app/product-feature/product-feature.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-feature',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-feature.component.html',
+  styleUrl: './product-feature.component.css'
+})
+export class ProductFeatureComponent {
+
+}
