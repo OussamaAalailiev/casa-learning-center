2977017325e4d679959e2b849e45da32801c104b
Aalaila Oussama <aalaila.oussama@gmail.com>
2024-04-17 23:58:37 +0100

    add event cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { ProductFeatureComponent } from './product-feature/product-feature.component';
+import { SectionEventsComponent } from './section-events/section-events.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ProductFeatureComponent],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    ProductFeatureComponent,
+    SectionEventsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
