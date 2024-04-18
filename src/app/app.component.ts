73fdd2c256e918a4c18ef8af9b43b90a19161274
OussamaAalailiev <aalaila.oussama@gmail.com>
2024-04-18 21:31:18 +0100

    home component added + <router-outlet> tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
-import { ProductFeatureComponent } from './product/product.component';
-import { SectionEventsComponent } from './section-events/section-events.component';
-import { FeatureComponent } from './feature/feature.component';
-import { NewsLetterComponent } from './news-letter/news-letter.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavComponent,
-    ProductFeatureComponent,
-    SectionEventsComponent,
-    FeatureComponent,
-    NewsLetterComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
